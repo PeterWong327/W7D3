212022eab31fc93ad56075c98c96e6c1ba94a720
peterwong327 <peterwongisnow@gmail.com>
2018-08-01 17:33:12 -0700

    Complete Pokemon through Phase 2: Store

--- pokedex/frontend/pokedex.jsx
+++ pokedex/frontend/pokedex.jsx
@@ -1,7 +1,19 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import configureStore from './store/store.js';
+
+
+import { fetchAllPokemon } from './util/api_util';
+import { receiveAllPokemon } from './actions/pokemon_actions';
 
 document.addEventListener("DOMContentLoaded", () => {
+
+  window.fetchAllPokemon = fetchAllPokemon;
+  window.receiveAllPokemon = receiveAllPokemon;
+  const store = configureStore();
+  window.getState = store.getState;
+  window.dispatch = store.dispatch;
+
   const rootEl = document.getElementById('root');
   ReactDOM.render(<h1>Pokedex</h1>, rootEl);
 });
